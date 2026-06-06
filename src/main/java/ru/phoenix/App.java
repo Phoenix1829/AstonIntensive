30cd2622a5bac104f03e5c4b71062ccb9f864f7d
Phoenix1829 <mishakonshu03@gmail.com>
2026-06-07 00:45:40 +0600

    Complete task 1

--- src/main/java/ru/phoenix/App.java
+++ src/main/java/ru/phoenix/App.java
@@ -1,13 +1,33 @@
 package ru.phoenix;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import ru.phoenix.task1.ChainedHashMap;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ChainedHashMap<String, Integer> map = new ChainedHashMap<>();
+
+        map.put("Mikhail", 10);
+        map.put("Stanislav", 20);
+        map.put("Roman", 30);
+
+        System.out.println("Map: " + map);
+
+        System.out.println("Size: " + map.size());
+
+        System.out.println("Stanislav = " + map.get("Stanislav"));
+
+        System.out.println("Contains: "
+                + map.containsKey("Roman"));
+
+        map.remove("Mikhail");
+
+        System.out.println("After remove: " + map);
+
+        System.out.println("Size: " + map.size());
+
+        System.out.println("Is empty: "
+                + map.isEmpty());
     }
 }
